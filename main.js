4b1d5fdce5e14b9aca3ef82439995ea3373db375
tushar-9015 <109728513+tushar-9015@users.noreply.github.com>
2023-08-27 03:48:14 +0530

    commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const con = document.querySelector("#container");
+const love = document.querySelector("i");
+
+con.addEventListener('dblclick', ()=> {
+    love.style.transform = "translate(-50%, -50%) scale(1)";
+    love.style.opacity =0.8;
+
+    setTimeout(()=>{
+        love.style.opacity = 0
+    },1000)
+
+    setInterval(()=>{
+        love.style.transform = "translate(-50%, -50%) scale(0)";
+    },2000)
+
+})
